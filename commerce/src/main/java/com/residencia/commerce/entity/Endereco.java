6f6edcbb150c9c1875365f20ed80bccad3af2c17
Bernardo-Gutman-Siqueira <102383550+Bernardo-Gutman-Siqueira@users.noreply.github.com>
2022-05-31 22:36:59 -0300

    Update Endereco.java

--- commerce/src/main/java/com/residencia/commerce/entity/Endereco.java
+++ commerce/src/main/java/com/residencia/commerce/entity/Endereco.java
@@ -42,6 +42,71 @@ public class Endereco {
 	
 	@Column(name = "uf")
 	private String uf;
+
+	public Integer getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(Integer idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
 	
-	//Getter and Setter
+	
+	}
 }
