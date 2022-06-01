ce7049ada616a94c101f911592baf108598baf8c
Aysha Alessandra <102393292+AyshaAlessandra@users.noreply.github.com>
2022-05-31 21:05:40 -0300

    Update Categoria.java
    adicionei classe categoria

--- commerce/src/main/java/com/residencia/commerce/entity/Categoria.java
+++ commerce/src/main/java/com/residencia/commerce/entity/Categoria.java
@@ -1,5 +1,49 @@
 package com.residencia.commerce.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
 public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_categoria")
+	private Integer idCategoria;
+
+	@Column(name = "nome")
+	private String nomeCategoria;
+
+	@Column(name = "descricao")
+	private String descricaoCategoria;
+
+	public Integer getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Integer idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public String getDescricaoCategoria() {
+		return descricaoCategoria;
+	}
+
+	public void setDescricaoCategoria(String descricaoCategoria) {
+		this.descricaoCategoria = descricaoCategoria;
+	}
 
 }
